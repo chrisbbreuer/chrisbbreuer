3f01e1c204a44f215c43d7f3f00461c61d14189c
Chris <chrisbreuer93@gmail.com>
2023-01-24 17:35:36 -0500

    chore: wip

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,15 +1,11 @@
 export function formatDate(dateString) {
-  console.log('1. dateString', dateString)
-
   if (dateString && isNaN(Date.parse(dateString))) // support for displaying "Coming Soon" date
     return dateString
 
-  const d = new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-US', {
+  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
     timeZone: 'UTC',
   })
-  console.log('2. d', d)
-  return d
 }
