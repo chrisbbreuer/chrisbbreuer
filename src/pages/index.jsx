5ad8a17dbec2cfea4928535dac7bb1c8e996a98e
Chris <chrisbreuer93@gmail.com>
2023-01-26 15:25:56 -0500

    chore: wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -193,7 +193,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="/resume.png" variant="secondary" className="group mt-6 w-full">
+      <Button href="/resume.png" variant="secondary" className="group mt-6 w-full" target="_blank">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
