8042d032a036d5143e7870c58b72c719ead19e27
Chris <chrisbreuer93@gmail.com>
2023-01-27 14:58:23 -0500

    fix: import fathom client

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
-
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import * as Fathom from 'fathom-client';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
