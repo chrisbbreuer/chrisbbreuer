87457dd92394f1422f3352db8f5c56f1e582ebd8
Chris <chrisbreuer93@gmail.com>
2023-01-27 14:51:21 -0500

    fix: 'router' has already been declared

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -19,8 +19,6 @@ function usePrevious(value) {
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
 
-  const router = useRouter();
-
   useEffect(() => {
     // Initialize Fathom when the app loads
     Fathom.load('RWXTXCQZ', {
