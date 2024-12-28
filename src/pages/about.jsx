be9777e070612432665888892eb05936e1d38a98
Chris <chrisbreuer93@gmail.com>
2024-12-28 16:42:06 +0100

    chore: adjust tagline

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -62,7 +62,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Chris Breuer. Passionately obsessed over Web DX.
+              I’m Chris Breuer. Passionately obsessed over Developer Tooling.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
